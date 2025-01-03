5e88a4859534e3d8cfa5da3cb54a1cba9d90c186
Szymon Czermak <SzyCzermak00@gmail.com>
2025-01-03 03:43:26 +0100

    FrontEnd and change name

--- clearage/lib/predictionDialog.dart
+++ clearage/lib/predictionDialog.dart
@@ -5,10 +5,12 @@ import 'styles.dart';
 class PredictionDialog extends StatelessWidget {
   final String predictionResult;
   final File selectedImage;
+  final bool isLoading; // Dodano parametr isLoading
 
   const PredictionDialog({
     required this.predictionResult,
     required this.selectedImage,
+    this.isLoading = false, // Domyślnie false
     super.key,
   });
 
@@ -45,14 +47,14 @@ class PredictionDialog extends StatelessWidget {
                 decoration: BoxDecoration(
                   border: Border.all(
                     color: predictionResult == "Dorosły (+18)"
-                        ? Colors.green
-                        : Colors.red,
+                        ? const Color.fromARGB(190, 76, 175, 79)
+                        : const Color.fromARGB(190, 244, 67, 54),
                     width: 4.0,
                   ),
-                  borderRadius: BorderRadius.circular(10.0),
+                  borderRadius: BorderRadius.circular(20.0),
                 ),
                 child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10.0),
+                  borderRadius: BorderRadius.circular(15.0),
                   child: Image.file(
                     selectedImage,
                     height: 370,
@@ -61,35 +63,42 @@ class PredictionDialog extends StatelessWidget {
                   ),
                 ),
               ),
-              const SizedBox(height: 20),
-              Text(
-                predictionResult,
-                style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop(); // Zamknij okno dialogowe
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 10,
+              const SizedBox(height: 30),
+              isLoading
+                  ? const Text(
+                      'Ładowanie, proszę czekać...',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    )
+                  : Text(
+                      predictionResult,
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+              const SizedBox(height: 30),
+              Container(
+                decoration: AppStyles.buttonDecoration,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(); // Zamknij okno dialogowe
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 45,
+                      vertical: 10,
+                    ),
+                    backgroundColor: Colors.transparent,
+                    shadowColor: const Color.fromARGB(255, 255, 255, 255),
+                    elevation: 0,
                   ),
-                  backgroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                child: const Text(
-                  'Zamknij',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black,
+                  child: const Text(
+                    'Zamknij',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
                   ),
                 ),
               ),
